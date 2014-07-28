7f97cb8506c1a3a5e1dc2ff6917beb14dc8e10c1
Scisco <alireza@developmentseed.org>
2014-07-28 17:32:13 -0400

    Added the script

---
+++ github-ssh.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+## Author: scisco
+
+echo "Please enter the github usernames separating them by comma and no spaces."
+echo "For example: user1,user2,user3"
+echo ""
+read users
+
+
+##############################
+## ADD SSH KEYS
+##############################
+
+## Convert Users into array
+IFS=',' read -a user_array <<< "$users"
+
+counter=0
+
+## Grab SSH keys from Github for specified users
+for user in "${user_array[@]}"; do
+	keys=$(curl https://api.github.com/users/$user/keys | jq '.[]' | jq -r '.key')
+
+	## Convert SSH Keys into Bash Array
+	while IFS= read -r line; do
+	  key_array[$counter]="$line"
+	  let counter=counter+1
+	done <<< "$keys"
+done
+
+
+## Add them to authorized_keys
+for key in "${key_array[@]}"; do
+	echo $key >> $setup_dir/.ssh/authorized_keys
+done
+
+## Fix authorized_keys permission
+chmod 644 $setup_dir/.ssh/authorized_keys
